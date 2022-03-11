48bfbe1f55aaa899f996c4df64ec21859f85db9f
Donatella7 <campaniellodonatella@gmail.com>
2022-03-11 20:42:31 +0100

    form

--- js/myscript.js
+++ js/myscript.js
@@ -31,22 +31,50 @@ const team = [
     },
 ];
 
-
-for (let i = 0; i < team.length; i++){
-    console.log("-------");
-    for (const key in team[i]) {
-        console.log(team[i][key]);
-    }
-}
+const parent = document.getElementById("my-img-container");
 
 // il for in non serve perchè qui la proprietà la prendo singolarmente 
 for (let i = 0; i < team.length; i++){
-    document.getElementById("my-img-container").innerHTML +=` 
-    <div class="my-card">
-        <img src=img/${team[i].image}  alt="img member">
-        <p class="my-name">${team[i].name}</p>
-        <p class="my-role">${team[i].role}</p>
+    parent.innerHTML +=` 
+    <div class="card my-card">
+    <img class="card-img-top" src=img/${team[i].image}  alt="img member">
+    <div class="card-body">
+    <h5 class="card-title">${team[i].name}</h5>
+    <p class="card-text">${team[i].role}</p>
+    </div>
     </div>
     `;
 }
 
+
+function createMember(username,role,image){
+    const newTeamMember = {
+        name:username,
+        role:role,
+        image:image,
+    }
+    
+    return newTeamMember;
+} 
+
+
+const btnSubmit = document.getElementById("submit");
+btnSubmit.addEventListener("click", function(){
+    
+    const nameNewMember = document.getElementById("user-name").value;
+    const roleNewMember = document.getElementById("user-role").value;
+    const imageNewMember = document.getElementById("user-url-image").value;
+    const newTeamMember = createMember(nameNewMember,roleNewMember,imageNewMember);
+    team.push(newTeamMember);
+    
+    parent.innerHTML += ` 
+    <div class="card my-card">
+    <img class="card-img-top" src=${imageNewMember}  alt="img member">
+    <div class="card-body">
+    <h5 class="card-title">${nameNewMember}</h5>
+    <p class="card-text">${roleNewMember}</p>
+    </div>
+    </div>
+    `;
+    
+});
